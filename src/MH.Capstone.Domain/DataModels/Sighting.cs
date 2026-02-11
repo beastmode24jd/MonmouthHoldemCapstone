2f9edb0bdc5382d947100f8e88387e5035570368
JD McShane <jmcshane22@wou.edu>
2026-02-10 17:32:39 -0800

    Added EF AppDbContext and the sightings EF Data Model/Entitity

---
+++ src/MH.Capstone.Domain/DataModels/Sighting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH.Capstone.Domain.DataModels
+{
+    [Table("Sighting")]
+    public partial class Sighting
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        [Column("Lat")]
+        public decimal Latitude { get; set; }
+
+        [Column("Long")]
+        public decimal Longitude { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; } = null;
+
+        // Commented out until User is defined by other developers
+        //[ForeignKey(nameof(UserId))]
+        //public virtual User User { get; set; } = null!;
+    }
+}
