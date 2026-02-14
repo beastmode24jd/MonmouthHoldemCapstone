2213d7e34b5e00789723eb2e8e2a9be4ea28cab4
JD McShane <jmcshane22@wou.edu>
2026-02-13 18:40:00 -0800

    Added class stuctures for both the SightingsService and the SightingsServiceTests

---
+++ src/MH.Capstone.Domain.Tests.Unit/Services/SightingsServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MH.Capstone.Domain.Tests.Unit.Services;
+
+[TestFixture]
+[ExcludeFromCodeCoverage]
+public class SightingsServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ src/MH.Capstone.Domain/Services/SightingsService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MH.Capstone.Domain.DataModels;
+
+namespace MH.Capstone.Domain.Services
+{
+    public interface ISightingsService
+    {
+        Task CreateSightingAsync();
+
+        Task<Sighting> GetSightingByIdAsync(Guid id);
+    }
+
+    public class SightingsService : ISightingsService
+    {
+        public async Task CreateSightingAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Sighting> GetSightingByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
