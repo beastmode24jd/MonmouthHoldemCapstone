b4385a12ea326064f0cf299f0287a228e299d808
Arin <aporter23@wou.edu>
2026-02-14 23:31:22 -0800

    Adding ImageService Injection to the Dashboard Controller.
    Also invoking the async method from Services within the Upload method, before returning the filepath to the Index View.

--- src/MH.Capstone.WebApp/Controllers/DashboardController.cs
+++ src/MH.Capstone.WebApp/Controllers/DashboardController.cs
@@ -11,17 +11,35 @@ namespace MH.Capstone.WebApp.Controllers
         // Logger to track dashboard access and activity. 
         private readonly ILogger<DashboardController> _logger;
 
+        // Dep. Injection for image upload feature.
+         private readonly IProfileImageService _imageService;
+
         // Constructor that injects the logger dependency
-        public DashboardController(ILogger<DashboardController> logger)
+        public DashboardController(ILogger<DashboardController> logger, IProfileImageService imageService)
         {
             _logger = logger;
+            _imageService = _imageService;
         }
 
         // Displays the main dashboard page for authenticated users. 
         public IActionResult Index()
         {
             _logger.LogInformation("User {Email} accessed dashboard", User.Identity?.Name);
+            // Default value, would fetch image URL from DB if properly connected.
+            ViewBag.ProfileImageUrl = null;
             return View();
+        }
+
+        public async Task<IActionResult> UploadProfileImage(IFormFile profilePic)
+        {
+            if (profilePic != null)
+            {
+                var imageUrl = await _imageService.UploadImageAsync(profilePic);
+                // Stubbed, would save 'imageUrl' to local DB otherwise?
+                _logger.LogInformation($"Image uploaded to {imageUrl}");
+            }
+            // Send this information back to the main dashboard page.
+            return RedirectToAction("Index");
         }
     }
 }
