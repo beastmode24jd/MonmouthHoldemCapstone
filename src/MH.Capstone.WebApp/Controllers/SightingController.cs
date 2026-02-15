f8c308a775b5f13749b51b5c7ee0f0ffe2e0ed42
JD McShane <jmcshane22@wou.edu>
2026-02-15 06:21:40 -0800

    Added Required input tag helper to WebApp

--- src/MH.Capstone.WebApp/Controllers/SightingController.cs
+++ src/MH.Capstone.WebApp/Controllers/SightingController.cs
@@ -1,4 +1,6 @@
-﻿using MH.Capstone.Domain.Services;
+﻿using System.Net;
+using MH.Capstone.Domain.DataModels;
+using MH.Capstone.Domain.Services;
 using MH.Capstone.WebApp.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -12,12 +14,14 @@ namespace MH.Capstone.WebApp.Controllers
     {
         private readonly ILogger<SightingController> _logger;
         private readonly ISightingsService _sightingsService;
-        //private readonly UserManager<ApplicationUser>
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public SightingController(ILogger<SightingController> logger, ISightingsService sightingsService)
+        public SightingController(ILogger<SightingController> logger, ISightingsService sightingsService,
+            UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
             _sightingsService = sightingsService;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -36,11 +40,19 @@ namespace MH.Capstone.WebApp.Controllers
         {
             if (!ModelState.IsValid)
             {
+                _logger.LogInformation("Invalid sighting model was submitted and rejected.");
                 return View(sightingUpload);
             }
 
-            // TODO: Get the user ID from the logged in user using the UserManager
-            var dataModel = sightingUpload.ToDataModel(Guid.Empty);
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                _logger.LogError("Authenticated user could not be found in the database.");
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+
+            var dataModel = sightingUpload.ToDataModel(Guid.Parse(user.Id));
             await _sightingsService.CreateSightingAsync(dataModel);
             return RedirectToAction("Index", "Dashboard", new { sighting_success = true });
         }
