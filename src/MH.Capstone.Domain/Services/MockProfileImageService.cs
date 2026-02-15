1ba519592cc33868f4ad7dec1754c9a6abab179f
Arin <aporter23@wou.edu>
2026-02-14 23:19:11 -0800

    Fleshed out MockProfileImage Service.
    Wrote the GUID and local filestream for saving the file to wwwroot. This will change with the proper DB implementation.

--- src/MH.Capstone.Domain/Services/MockProfileImageService.cs
+++ src/MH.Capstone.Domain/Services/MockProfileImageService.cs
@@ -1,9 +1,30 @@
 public class MockProfileImageService : IProfileImageService
 {
+    /* Mocked Service for storing profile image uploads.
+        Writes to local files, and can have implementation changed for
+            saving to a production DB.
+    */
     private readonly string _storagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads/profiles");
 
     public async Task<string> UploadImageAsync(IFormFile file)
     {
-        return "Not implemented yet";
+        if (file.Length == 0 || file == null ) return null;
+
+        // Ensure that the path to the image file exists, if image file is present
+        if (!Directory.Exists(_storagePath))
+        {
+            Directory.CreateDirectory(_storagePath);
+        }
+
+        // Use GUID (random character string generator?) to create unique file name
+        var filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+        var filepath = Path.Combine(_storagePath, filename);
+
+        using (var stream = new FileStream(filepath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        return $"/uploads/profiles/{filename}";
     }
 }
