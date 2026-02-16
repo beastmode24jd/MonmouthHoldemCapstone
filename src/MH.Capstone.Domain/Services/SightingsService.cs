032fe3385881b71fda1465ff279812b2f547fdc3
JD McShane <jmcshane22@wou.edu>
2026-02-16 08:30:55 -0800

    Removed logger message that failed some frailge Image Unit Tests for SightingService

--- src/MH.Capstone.Domain/Services/SightingsService.cs
+++ src/MH.Capstone.Domain/Services/SightingsService.cs
@@ -58,10 +58,6 @@ namespace MH.Capstone.Domain.Services
 
         public bool ValidateImage(IFormFile? imageBuffer)
         {
-            _logger.LogInformation($"Null? {imageBuffer == null}\n" +
-                                   $"Length: {imageBuffer?.Length}\n" +
-                                   $"Type: {imageBuffer?.ContentType}");
-
             // Null check - if null, we are not valid
             if (imageBuffer == null)
             {
