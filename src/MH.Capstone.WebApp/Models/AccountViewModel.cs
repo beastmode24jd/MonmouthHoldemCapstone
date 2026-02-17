a950e69d9e8f02779e7c5649314820b8c5ca07fd
JD McShane <jmcshane22@wou.edu>
2026-02-17 13:40:56 -0800

    Added blank account/user page and a basic AccountVM

---
+++ src/MH.Capstone.WebApp/Models/AccountViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MH.Capstone.Domain.DataModels;
+
+namespace MH.Capstone.WebApp.Models
+{
+    public class AccountViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string ProfileImageUrl { get; }
+
+        public AccountViewModel(ApplicationUser user)
+        {
+            Id = Guid.Parse(user.Id);
+            Name = user.UserName;
+            Email = user.Email;
+            ProfileImageUrl = user.GetProfileImageUrl();
+        } 
+    }
+}
