836270de433502b9529df715dc04c049eae398bf
JD McShane <jmcshane22@wou.edu>
2026-02-24 14:06:15 -0800

    Added InApp concrete notification service class skeleton

---
+++ src/MH.Capstone.Domain/Services/Notifications/InAppNotificationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MH.Capstone.Domain.DataModels;
+using MH.Capstone.Domain.Services.Abstraction;
+
+namespace MH.Capstone.Domain.Services.Notifications
+{
+    public class InAppNotificationService : INotificationService
+    {
+        public async Task SendNotificationAsync(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Notification>> GetPendingNotificationsAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Notification>> GetAllNotificationsAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task MarkNotificationAsReadAsync(Guid notificationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
