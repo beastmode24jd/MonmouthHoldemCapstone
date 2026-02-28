3f0ac340a61031e2c41e51527e2fc36c30b0d080
JD McShane <jmcshane22@wou.edu>
2026-02-28 14:14:40 -0800

    Added generic model save method to allow for non-anemic models

---
+++ src/MH.Capstone.Domain/DataModels/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MH.Capstone.Domain.DataAccess;
+using MH.Capstone.Domain.DataAccess.Repositories;
+
+namespace MH.Capstone.Domain.DataModels
+{
+    public static class Extensions
+    {
+        public static async Task SaveModelAsync<TModel>(this TModel model,
+            IRepository<TModel, ApplicationDbContext> repo) where TModel : class, new()
+                => await repo.AddOrUpdateAsync(model);
+    }
+}
