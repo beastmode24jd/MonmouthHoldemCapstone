d004b3d5b90f4adfc81bd4e8247d7134a5cafa4d
JD McShane <jmcshane22@wou.edu>
2026-03-01 15:12:21 -0800

    Added ApiDtos (contracts) for the Ninjas Animal API

---
+++ src/MH.Capstone.Domain/ApiContracts/Ninjas/NinjasAnimalApiDto.cs
@@ -0,0 +1,41 @@
+﻿namespace MH.Capstone.Domain.ApiContracts.Ninjas
+{
+    public record AnimalApiDto(string name, AnimalApiTaxonomyDto taxonomy, 
+        AnimalApiLocations locations, AnimalApiCharacteristics characteristics)
+    { }
+
+    public record AnimalApiTaxonomyDto(string kingdom, string phylum,
+        string taxClass, string order, string family, string genus,
+        string scientificName)
+    { }
+
+    public record AnimalApiLocations(string[] locations)
+    { }
+
+    public record AnimalApiCharacteristics(
+        string prey,
+        string nameOfYoung,
+        string groupBehavior,
+        int estimatedPopulationSize,
+        string biggestThreat,
+        string mostDistinctiveFeature,
+        string gestationPeriod,
+        string habitat,
+        string diet,
+        int averageLitterSize,
+        string lifestyle,
+        string commonName,
+        int numberOfSpecies,
+        string location,
+        string slogan,
+        string group,
+        string color,
+        string skinType,
+        string topSpeed,
+        string lifespan,
+        string weight,
+        string height,
+        string ageOfSexualMaturity,
+        string ageOfWeaning)
+    { }
+}
