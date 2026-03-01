26a71126f54255418aac9aee8c90bcbd184d05d7
Marquis Bowles <marquis.bowles@gmail.com>
2026-02-28 19:47:00 -0800

    GREEN: GetLeaderboardPageAsync returns users ordered by points descending

--- src/MH.Capstone.Domain/Services/LeaderboardService.cs
+++ src/MH.Capstone.Domain/Services/LeaderboardService.cs
@@ -1,6 +1,7 @@
 using MH.Capstone.Domain.DataAccess;
 using MH.Capstone.Domain.DataModels;
 using MH.Capstone.Domain.Services.Abstraction;
+using Microsoft.EntityFrameworkCore;
 
 namespace MH.Capstone.Domain.Services
 {
@@ -13,8 +14,15 @@ namespace MH.Capstone.Domain.Services
             _context = context;
         }
 
-        public Task<List<ApplicationUser>> GetLeaderboardPageAsync(int page, int pageSize = 30)
-            => throw new NotImplementedException();
+        public async Task<List<ApplicationUser>> GetLeaderboardPageAsync(int page, int pageSize = 30)
+        {
+            return await _context.Users
+                .Where(u => !u.IsDeactivated) // deactivated users should not appear on the leaderboard
+                .OrderByDescending(u => u.Points) // sort by points descending
+                .Skip((page - 1) * pageSize) // skip previous pages
+                .Take(pageSize) // limit to page size
+                .ToListAsync();
+        }
 
         public Task<int> GetTotalUserCountAsync()
             => throw new NotImplementedException();
