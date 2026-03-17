a10a28525d84650150297ff2178502f043c368a3
JD McShane <jmcshane22@wou.edu>
2026-03-17 16:49:52 -0700

    Added EmailQueue EF Migration

---
+++ src/MH.Capstone.Domain/Migrations/20260317234648_AddEmailQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MH.Capstone.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailQueue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmailQueue",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Recipient = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    HtmlBody = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PlainTextBody = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ScheduledAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    IsSent = table.Column<bool>(type: "bit", nullable: false),
+                    SentAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    Attempts = table.Column<int>(type: "int", nullable: false),
+                    LastAttemptAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LastError = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Processing = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailQueue", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailQueue_IsSent_ScheduledAt",
+                table: "EmailQueue",
+                columns: new[] { "IsSent", "ScheduledAt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailQueue");
+        }
+    }
+}
