21d0db4a97c173511b13e939da3896fea634f8b4
Arin <arin.porter@outlook.com>
2026-04-26 17:53:08 -0700

    Saving local branch changes before updating with upstream dev.

--- src/MH.Capstone.Tests.Acceptance/Features/CSP-124.feature.cs
+++ src/MH.Capstone.Tests.Acceptance/Features/CSP-124.feature.cs
@@ -107,7 +107,7 @@ namespace MH.Capstone.Tests.Acceptance.Features
         
         private static global::Reqnroll.Formatters.RuntimeSupport.FeatureLevelCucumberMessages InitializeCucumberMessages()
         {
-            return new global::Reqnroll.Formatters.RuntimeSupport.FeatureLevelCucumberMessages("Features/CSP-124.feature.ndjson", 4);
+            return new global::Reqnroll.Formatters.RuntimeSupport.FeatureLevelCucumberMessages("Features/CSP-124.feature.ndjson", 5);
         }
         
         [global::NUnit.Framework.TestAttribute()]
@@ -181,6 +181,47 @@ this.ScenarioInitialize(scenarioInfo, ruleInfo);
 #line hidden
 #line 21
     await testRunner.AndAsync("see the new club on my Clubs page", ((string)(null)), ((global::Reqnroll.Table)(null)), "And ");
+#line hidden
+            }
+            await this.ScenarioCleanupAsync();
+        }
+        
+        [global::NUnit.Framework.TestAttribute()]
+        [global::NUnit.Framework.DescriptionAttribute("Alex has a new club, and wants to add Lily to it.")]
+        [global::NUnit.Framework.CategoryAttribute("clubs")]
+        public async global::System.Threading.Tasks.Task AlexHasANewClubAndWantsToAddLilyToIt_()
+        {
+            string[] tagsOfScenario = new string[] {
+                    "clubs"};
+            global::System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new global::System.Collections.Specialized.OrderedDictionary();
+            string pickleIndex = "2";
+            global::Reqnroll.ScenarioInfo scenarioInfo = new global::Reqnroll.ScenarioInfo("Alex has a new club, and wants to add Lily to it.", null, tagsOfScenario, argumentsOfScenario, featureTags, pickleIndex);
+            string[] tagsOfRule = ((string[])(null));
+            global::Reqnroll.RuleInfo ruleInfo = null;
+#line 24
+this.ScenarioInitialize(scenarioInfo, ruleInfo);
+#line hidden
+            if ((global::Reqnroll.TagHelper.ContainsIgnoreTag(scenarioInfo.CombinedTags) || global::Reqnroll.TagHelper.ContainsIgnoreTag(featureTags)))
+            {
+                await testRunner.SkipScenarioAsync();
+            }
+            else
+            {
+                await this.ScenarioStartAsync();
+#line 25
+    await testRunner.GivenAsync("I am on the Clubs page", ((string)(null)), ((global::Reqnroll.Table)(null)), "Given ");
+#line hidden
+#line 26
+    await testRunner.WhenAsync("I select valid options", ((string)(null)), ((global::Reqnroll.Table)(null)), "When ");
+#line hidden
+#line 27
+    await testRunner.AndAsync("I click the Create New Club button", ((string)(null)), ((global::Reqnroll.Table)(null)), "And ");
+#line hidden
+#line 28
+    await testRunner.ThenAsync("I should be able to invite another user", ((string)(null)), ((global::Reqnroll.Table)(null)), "Then ");
+#line hidden
+#line 29
+    await testRunner.AndAsync("they should see the Club invite on their Clubs page", ((string)(null)), ((global::Reqnroll.Table)(null)), "And ");
 #line hidden
             }
             await this.ScenarioCleanupAsync();
